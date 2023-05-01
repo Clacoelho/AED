292d3a37a426ed13f6ad5d731e8236e01bb5e2dc
Clacoelho <127640830+Clacoelho@users.noreply.github.com>
2023-05-01 20:38:16 -0300

    Add files via upload

---
+++ EP05-Insertionsort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+//Função retirada do livro indicado
+void Inserção (int n, int v[]) {
+int i, j, x;
+for (j = 1; /*A*/ j < n; j++) {
+x = v[j];
+for (i = j-1; i >= 0 && v[i] > x; i--)
+v[i+1] = v[i];
+v[i+1] = x;
+}
+}
+
+int main() {
+    int i, n = 5000;
+    int v[n];//declara de forma manual o número máximo de elementos do vetor.
+    clock_t tempo_inicial, tempo_final;
+    double tempo_total;
+    
+    srand(time(NULL));
+
+    // Lê o tamanho do vetor e seus elementos. 
+    for (i = 0; i < n; i++) {
+        v[i] = rand() % 5000;
+    }
+    
+    tempo_inicial = clock();
+
+    // Chama a função de ordenação
+    Inserção(n, v);
+    
+    tempo_final = clock();
+    
+    // cálculo do tempo total e exibição do vetor ordenado
+    tempo_total = (double) (tempo_final - tempo_inicial) / CLOCKS_PER_SEC;
+    printf("Tempo total de execução: %f segundos\n", tempo_total);
+
+
+    // Imprime o vetor ordenado
+    for (i = 0; i < n; i++) {
+        printf("%d ", v[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
